d8bb1096e6970a049dc10982b56dcba78624fbf4
Jackson Luiz <jacksoncuiaba99@gmail.com>
2024-10-25 01:17:40 -0400

    Create rotas para Os Componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+import { FirstComponentComponent } from "./components/first-component/first-component.component";
+import { ListRenderComponent } from "./components/list-render/list-render.component";
+import { EmitterComponent } from "./components/emitter/emitter.component"; 
+import { ChangerNumberComponent } from "./components/changer-number/changer-number.component";
+import { DirectivesComponent } from "./components/directives/directives.component";
+import { EventosComponent } from "./components/eventos/eventos.component";
+import { IfRenderComponent } from "./components/if-render/if-render.component";
+import { ParentDataComponent } from "./components/parent-data/parent-data.component";
+import { PipesComponent } from "./components/pipes/pipes.component";
+import { TowWayBindingComponent } from "./components/tow-way-binding/tow-way-binding.component";
+
+const routes: Routes = [
+    { path: '', component: FirstComponentComponent },
+    { path: 'list', component: ListRenderComponent },
+    { path: 'emitter', component: EmitterComponent },
+    { path: 'chager', component: ChangerNumberComponent},
+    { path: 'directives', component: DirectivesComponent},
+    { path: 'directives', component: DirectivesComponent},
+    { path: 'first', component: FirstComponentComponent},
+    { path: 'eventos', component: EventosComponent},
+    { path: 'if', component: IfRenderComponent},
+    { path: 'parent', component: ParentDataComponent},
+    { path: 'pipes', component: PipesComponent},
+    { path: 'tow', component: TowWayBindingComponent},
+  ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
-import { DirectivesComponent } from './componets/directives/directives.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 import { IfRenderComponent } from './components/if-render/if-render.component';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { EmitterComponent } from './components/emitter/emitter.component';
