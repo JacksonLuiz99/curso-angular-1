a99138b9362b589899d2ddcc2f3c1314fb834092
Jackson Luiz <jacksoncuiaba99@gmail.com>
2024-10-24 21:39:05 -0400

    Create Interface
    Criando uma interface que exiba o nome do personagem e sua idade

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent implements OnInit {
-  animals = [
-    {name: "Spike", type: 'Cão'},
-    {name: "Tom", type: 'Gato'},
-    {name: "Jerry", type: 'Rato'},
-    {name: "Quacker", type: 'Pato'},
+  animals: Animal[] = [
+    {name: "Tom", type: 'Gato', age: 5},
+    {name: "Jerry", type: 'Rato', age: 3},
+    {name: "Spike", type: 'Bulldog', age: 6},
+    {name: "Tyke", type: 'Filhote de Bulldog', age: 1},
+    {name: "Quacker", type: 'Filhote de Pato', age: 2},
   ];
+
+  animal: Animal = {
+    name: 'Teste',
+    type: 'Alguma Coisa',
+    age: 10,
+  }
+  animalDetails = ``;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  showAge(animal: Animal): void {
+    this.animalDetails = `O Personagem ${animal.name} tem ${animal.age} anos`;
   }
 
 }
