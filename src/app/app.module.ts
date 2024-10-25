84b1dce3fa8e43462c04e5d04cbee7366a43b9b8
Jackson Luiz <jacksoncuiaba99@gmail.com>
2024-10-24 22:46:35 -0400

    Two-Way Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { EmitterComponent } from './components/emitter/emitter.component';
 import { ChangerNumberComponent } from './components/changer-number/changer-number.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
 import { PipesComponent } from './components/pipes/pipes.component';
+import { TowWayBindingComponent } from './components/tow-way-binding/tow-way-binding.component';
 
 @NgModule({
   declarations: [
@@ -24,10 +26,11 @@ import { PipesComponent } from './components/pipes/pipes.component';
     EmitterComponent,
     ChangerNumberComponent,
     ListRenderComponent,
-    PipesComponent
+    PipesComponent,
+    TowWayBindingComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, FormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/components/tow-way-binding/tow-way-binding.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tow-way-binding',
+  templateUrl: './tow-way-binding.component.html',
+  styleUrls: ['./tow-way-binding.component.css']
+})
+export class TowWayBindingComponent implements OnInit {
+  name: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
